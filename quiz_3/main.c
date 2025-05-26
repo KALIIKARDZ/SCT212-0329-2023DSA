bf4e09b067c2a08b51497f181eced35dbaa2c9ce
KALIIKARDZ <145854310+KALIIKARDZ@users.noreply.github.com>
2025-05-26 11:34:52 +0300

    Update main.c
    QUIZ 3

--- quiz_3/main.c
+++ quiz_3/main.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 bool containsDuplicate(int nums[], int size) {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) { 
         for (int j = i + 1; j < size; j++) {
             if (nums[i] == nums[j]) {
                 return true;
